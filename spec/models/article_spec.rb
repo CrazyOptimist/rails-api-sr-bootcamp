856b9db5b85d1f1c97ea5d9e054162e2aaa810dc
crazyoptimist <crazyoptimist@mail.com>
2022-01-18 14:30:30 -0600

    add a validation and make test green

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,13 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  it 'tests that factory is valid' do
-    article = build(:article) # because build(in-memory) is faster than create which persists the sample
-    expect(article).to be_valid
-  end
+  describe '#validations' do
+    it 'tests that factory is valid' do
+      article = build(:article) # because build(in-memory) is faster than create which persists the sample
+      expect(article).to be_valid
+    end
 
-  it 'has an invalid title' do
-    article = build(:article, title: '')
-    expect(article).not_to be_valid
+    it 'has an invalid title' do
+      article = build(:article, title: '')
+      expect(article).not_to be_valid
+    end
   end
 end
