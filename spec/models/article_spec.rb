437a524f463b3bdd90899ae739a90fa4434c9168
crazyoptimist <crazyoptimist@mail.com>
2022-01-18 10:46:31 -0600

    create the first test example using FactoryBot

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "tests article object" do
+    # article = Article.create({title: 'Simple Article', content: 'Sample Content'})
+    article = create(:article) # == FactoryBot.create (Because we configured factory bot in rails_helper.rb)
+    expect(article.title).to eq('Sample article')
+  end
 end
